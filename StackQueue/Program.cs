a26bb777affbf0dbbbdabbc407061b7bdb984cc8
unknown <kinekarpallavi21@gmail.com>
2022-03-28 18:31:20 +0530

    Create stack using linked list

---
+++ StackQueue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace StackQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Node firstNode = new Node(70);
+            Node secondNode = new Node(30);
+            Node thirdNode = new Node(56);
+
+            Stack stack = new Stack();  //Stack
+            stack.Push(firstNode);
+            stack.Push(secondNode);
+            stack.Push(thirdNode);
+            stack.DisplayStack();
+
+
+        }
+
+    }
+}
---
+++ StackQueue/Stack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueue
+{
+     class Stack
+    {
+        LinkedList linkedList;
+        public Stack()
+        {
+            linkedList = new LinkedList();
+        }
+        public void Push(Node node)
+        {
+            linkedList.AddNode(node);
+        }
+        public void DisplayStack()
+        {
+            
+            linkedList.Display();
+        }
+        public void Top()
+        {
+            var item = linkedList.Head;
+            Console.WriteLine(item.data);
+        }
+    }
+}
